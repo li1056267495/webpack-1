52d9c9f2f339abd3d310ed8eb891294b8675e779
Li <552484151@qq.com>
2020-07-17 09:46:31 +0800

    准备上传

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
 import x from './x.js'
+import png from './assets/1.png'
+console.log(png)
 console.log(x)
+
+const id = document.getElementById('app')
+id.innerHTML = `
+<img src="${png}">
+`
+
+
+const button = document.createElement('button')
+button.innerText = "懒加载"
+button.onclick = () => {
+    const promise = import("./lazy.js")
+    promise.then((module) => {
+        const fn = module.default
+        fn()
+    }, () => {
+        console.log('模块加载错误')
+    })
+}
+
+
+id.appendChild(button)
